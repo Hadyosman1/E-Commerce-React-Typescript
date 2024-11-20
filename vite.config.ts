80165e676e6337d768e8e8e48ecf7ba460a22c98
hady <osmanhady73@gmail.com>
2024-11-20 11:17:10 +0200

    Try to fix the bug.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
